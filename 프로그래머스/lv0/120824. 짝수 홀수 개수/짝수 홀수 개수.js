dbccf5f5110e0da0038788ab49366d7214e6298a
Koo <102615401+KooAme@users.noreply.github.com>
2022-11-10 02:50:46 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    var answer = [];
+    let even = 0; 
+    let odd = 0;
+    for(let i = 0; i < num_list.length; i++){
+        if(num_list[i] % 2 === 0){
+            even ++;
+        }
+        if(num_list[i] % 2 === 1){
+            odd ++;
+        }
+    }
+    answer.push(even, odd);
+    return answer;
+}
