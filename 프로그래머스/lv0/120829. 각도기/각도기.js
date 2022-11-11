ac32ad3f23917d249577d148fa5d2f36ff3d4aba
Koo <102615401+KooAme@users.noreply.github.com>
2022-11-11 21:58:53 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -8,7 +8,6 @@ function solution(angle) {
     if(angle > 90  && angle < 180){
         return 3;
     }
-    if(angle === 180){
-        return 4;
-    }
+    return 4;
 }
+//return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
