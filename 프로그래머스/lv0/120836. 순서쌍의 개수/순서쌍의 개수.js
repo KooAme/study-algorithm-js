f0613f86b50cbd365f2d2a6ceec56439b1d349f3
Koo <102615401+KooAme@users.noreply.github.com>
2022-11-13 11:25:53 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.78 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,14 +1,19 @@
 function solution(n) {
     var answer = 0;
-    let cnt = 0;
-    while(true){
-        if(cnt > n){
-            break;
-        }
-        if(n % cnt === 0){
+    // let cnt = 0;
+    // while(true){
+    //     if(cnt > n){
+    //         break;
+    //     }
+    //     if(n % cnt === 0){
+    //         answer = answer + 1;
+    //     }
+    //     cnt ++;
+    // }
+    for(let i = 0; i <= n; i++){
+        if(n % i === 0){
             answer = answer + 1;
         }
-        cnt ++;
     }
     return answer;
 }
