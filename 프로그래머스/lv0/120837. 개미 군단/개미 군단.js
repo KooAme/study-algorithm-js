eb71f7f4d66fbdc25d17e926756f0bfb0fb3659e
Koo <102615401+KooAme@users.noreply.github.com>
2022-11-19 23:47:38 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -3,4 +3,6 @@ function solution(hp) {
     let second = Math.floor((hp % 5) / 3);
     let last = Math.floor(((hp % 5) % 3) / 1);
     return first + second + last;
+    
+    //return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + (hp % 5) % 3;
 }
