5b24d02ab3b94872aec939fe597a5164465dbf77
Koo <102615401+KooAme@users.noreply.github.com>
2022-12-13 23:38:39 +0900

    [level 0] Title: 7의 개수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,1 +1,13 @@
-const solution = array => array.join("").split("").filter(a => a === '7').length;
+function solution(array) {
+    let result = 0;
+    
+    array.map((item) => {
+        while(item > 0) {
+            if(item % 10 === 7) {
+                result += 1;   
+            }
+            item = Math.floor(item/10);
+        }
+    });
+    return result;
+}
