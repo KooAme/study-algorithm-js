a813921da011d52d0729bb8f0dd27ee4521fe88c
Koo <dg.changeworld@gmail.com>
2023-05-12 01:47:27 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let a = input[0];
+let b = input[1];
+
+x_1 = b[2]; // 1의자리
+x_2 = b[1]; // 10의자리
+x_3 = b[0]; // 100의자리
+
+console.log(Number(a) * Number(x_1));
+console.log(Number(a) * Number(x_2));
+console.log(Number(a) * Number(x_3));
+console.log(Number(a) * Number(b));
